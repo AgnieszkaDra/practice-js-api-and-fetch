baf1f0d8e29b6a8914d595e50ea61d4432e3acb9
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2022-12-28 22:24:02 +0100

    end task 03

--- 03/app.js
+++ 03/app.js
@@ -2,4 +2,33 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     console.log('DOM');
-}
+
+} 
+    const button = document.querySelector('button')
+    const span = document.querySelector('span')
+
+    if(button) {
+        button.addEventListener('click', showId);
+    }
+
+    function showId() {
+        
+        const promise = fetch('https://api.ipify.org?format=json')
+
+         promise
+            .then(resp => {
+                if(resp.ok){
+                    
+                    return resp.json();
+                }
+                return Promise.reject(resp);
+            }
+                )
+            .then(data => {
+                if(span){
+                    return span.innerText = data.ip
+                }
+            })
+            //.catch(err => console.error(err));  
+    }
+    
